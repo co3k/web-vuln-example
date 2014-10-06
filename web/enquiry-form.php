214951338d1857ae655740356f92f8fa0106676f
Kousuke Ebihara <kousuke@co3k.org>
2014-10-06 21:38:33 +0900

    added an example of basic xss

---
+++ web/enquiry-form.php
@@ -0,0 +1,20 @@
+<?php
+header('Content-Type: text/html;charset=utf-8');
+
+if ('POST' === $_SERVER['REQUEST_METHOD']) {
+    echo '<p>問い合わせ内容を送信しました。</p>';
+    exit;
+}
+?>
+<!DOCTYPE html>
+<meta charset="utf-8" />
+<link rel="stylesheet" href="/example.css">
+<title>問い合わせフォーム</title>
+<h1>問い合わせフォーム</h1>
+<form action="./enquiry-form.php" method="post">
+<dl>
+<dt>お問い合わせ内容</dt>
+<dd><textarea style="width: 300px; height: 300px;"><?php print isset($_GET['msg']) ? $_GET['msg'] : '' ?></textarea></dd>
+</dl>
+<p><input type="submit" value="送信" /></p>
+</form>
